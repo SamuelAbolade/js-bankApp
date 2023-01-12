54f7d6663af1f6a091da8386b4e5da0216980cbf
TECHIE SAM <blessingabolade15@gmail.com>
2023-01-12 14:15:48 +0100

    Added chart feature to display user analysis

---
+++ js/analytics.js
@@ -0,0 +1,23 @@
+var xValues = ["Out", "In"];
+var yValues = [1, 1];
+var barColors = [
+    "#b91d47",
+    "#00aba9",
+];
+
+new Chart("analyticsChart", {
+    type: "doughnut",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+    },
+    options: {
+        title: {
+            display: false,
+            text: "Analytics chart"
+        }
+    }
+});
