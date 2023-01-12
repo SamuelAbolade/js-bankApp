afed0e7bdddb1c62124decc0b5ab6e9fab194cdb
TECHIE SAM <blessingabolade15@gmail.com>
2023-01-12 09:18:54 +0100

    Added change Password feature working correctly

--- js/change-password.js
+++ js/change-password.js
@@ -5,21 +5,48 @@ if (localStorage.usersList && localStorage.currentUser) {
     window.location = "index.html"
 }
 
-var oldPassword = 0
+var passwords = []
 var enteredOldPassword = false
+var enterdedNewPassword = false
+
 const changePassword = () => {
-    if (enteredOldPassword == false) {
-        // passwords.push(passwordTyped.value)
-        oldPassword = passwordTyped.value
+    let validatePassword = /^([\w]{1,10})+$/
+    let checkPasswordValidation = validatePassword.test(passwordTyped.value)
+    if (passwords.length == 0 && checkPasswordValidation) {
+        passwords.push(passwordTyped.value)
         passwordToEnter.innerText = "Enter your New Password"
         enteredOldPassword = true
         passwordTyped.value = ""
-        console.log(oldPassword)
-    } else if (enteredOldPassword) {
-        if(passwordTyped.value == onlineUser.password){
-            alert("There")
+        console.log(passwords)
+    } else if (enteredOldPassword && enterdedNewPassword==false && passwords.length==1 && checkPasswordValidation){
+        passwords.push(passwordTyped.value)
+        passwordToEnter.innerText = "Confirm your New Password"
+        enterdedNewPassword = true
+        passwordTyped.value = ""
+        console.log(passwords)
+    }else if (enterdedNewPassword && enteredOldPassword && passwords.length==2 && checkPasswordValidation){
+        passwords.push(passwordTyped.value)
+        console.log(passwords)
+        passwordTyped.value = ""
+        if(passwords[1]== passwords[2] && passwords[0]==onlineUser.password){
+            onlineUser.password = passwords[2]
+            localStorage.setItem("usersList", JSON.stringify(allUsers))
+            localStorage.setItem('currentUser', JSON.stringify(onlineUser))
+            console.log(onlineUser.password)
+            alert("Yay")
         }else{
-            alert("Incorrect")
+            passwordToEnter.innerText = "Enter your New Password"
+            passwords = []
+            enteredOldPassword = false
+            enterdedNewPassword = false
+            // var enteredOldPassword = false
+            // var enterdedNewPassword = false
+            alert("Noo")
         }
+    }
+    if(checkPasswordValidation==false){
+        passwordTyped.classList.add("is-invalid")
+    }else{
+        passwordTyped.classList.remove("is-invalid")
     }
 }
