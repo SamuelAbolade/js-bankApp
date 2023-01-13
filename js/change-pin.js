d4e3cb27a4dcea14f7ecad7fe058fb525830d5b2
TECHIE SAM <blessingabolade15@gmail.com>
2023-01-13 13:15:01 +0100

    Added interface to change-pin page

--- js/change-pin.js
+++ js/change-pin.js
@@ -19,39 +19,48 @@ pinTyped.addEventListener("input", () => {
 })
 console.log(onlineUser.transactionPin)
 const changePin = () => {
-    if (pins.length == 0 && pinTyped.value.length==4) {
+    if (pins.length == 0 && pinTyped.value.length == 4) {
         pins.push(pinTyped.value)
         pinTyped.value = ""
         console.log(pins)
         console.log("a")
         enteredOldPin = true
-        pinToEnter.innerText ="Enter your New 4-digit PIN"
-    } else if (enteredOldPin == true && enteredNewPin == false && confirmNewPin == false && pinTyped.value.length==4) {
+        pinToEnter.innerText = "Enter your New 4-digit PIN"
+    } else if (enteredOldPin == true && enteredNewPin == false && confirmNewPin == false && pinTyped.value.length == 4) {
         pins.push(pinTyped.value)
         pinTyped.value = ""
         console.log(pins)
         console.log("b")
         enteredNewPin = true
-        pinToEnter.innerText ="Confirm your New 4-digit PIN"
-    } else if (enteredOldPin == true && enteredNewPin == true && confirmNewPin == false && pinTyped.value.length==4) {
+        pinToEnter.innerText = "Confirm your New 4-digit PIN"
+    } else if (enteredOldPin == true && enteredNewPin == true && confirmNewPin == false && pinTyped.value.length == 4) {
         pins.push(pinTyped.value)
         pinTyped.value = ""
         console.log(pins)
         console.log("c")
         confirmNewPin = true
         if (pins[0] == onlineUser.transactionPin && pins[1] == pins[2]) {
-            alert("PIN Changed")
+            updateUser = allUsers.find((item, index) => item.accountnumber == onlineUser.accountnumber)
+            updateUser.transactionPin = onlineUser.transactionPin
             onlineUser.transactionPin = pins[2]
             localStorage.setItem("usersList", JSON.stringify(allUsers))
             localStorage.setItem('currentUser', JSON.stringify(onlineUser))
+            alert("PIN Changed")
+            changePinSuccess.innerHTML = `
+            <div class="text-center col-8 m-auto mt-5">
+                <i class="fa-solid fa-check fs-1 text-light p-3 rounded-circle" style="background-color:#590140"></i>
+                <h2 class="m-2 lh-base">Transaction-PIN successfully changed</h2>
+                <a href="/html/dashboard.html" class="btn text-light" style="background-color: #590140;">Go back to HOME</a>
+            </div>
+        `
             console.log(onlineUser.transactionPin)
         } else {
             alert("PINs does not match")
-            pins = [] 
+            pins = []
             enteredOldPin = false
             enteredNewPin = false
             confirmNewPin = false
-            pinToEnter.innerText ="Enter your old PIN"
+            pinToEnter.innerText = "Enter your old PIN"
         }
     }
 }
