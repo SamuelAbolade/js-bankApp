3096e90b74b90c2c963cc15834749b4edd2bca15
TECHIE SAM <blessingabolade15@gmail.com>
2023-01-13 16:05:20 +0100

    Added interface to change-password page

--- js/change-password.js
+++ js/change-password.js
@@ -2,7 +2,7 @@ if (localStorage.usersList && localStorage.currentUser) {
     allUsers = JSON.parse(localStorage.getItem("usersList"))
     onlineUser = JSON.parse(localStorage.getItem("currentUser"))
 } else {
-    window.location = "index.html"
+    window.location = "signup.html"
 }
 
 var passwords = []
@@ -10,7 +10,7 @@ var enteredOldPassword = false
 var enterdedNewPassword = false
 
 const changePassword = () => {
-    let validatePassword = /^([\w]{1,10})+$/
+    let validatePassword = /^([\w]{3,10})+$/
     let checkPasswordValidation = validatePassword.test(passwordTyped.value)
     if (passwords.length == 0 && checkPasswordValidation) {
         passwords.push(passwordTyped.value)
@@ -29,11 +29,19 @@ const changePassword = () => {
         console.log(passwords)
         passwordTyped.value = ""
         if(passwords[1]== passwords[2] && passwords[0]==onlineUser.password){
+            updateUser = allUsers.find((item, index) => item.accountnumber == onlineUser.accountnumber)
+            updateUser.password = onlineUser.password
             onlineUser.password = passwords[2]
             localStorage.setItem("usersList", JSON.stringify(allUsers))
             localStorage.setItem('currentUser', JSON.stringify(onlineUser))
             console.log(onlineUser.password)
-            alert("Yay")
+            alert("Password Chnaged")
+            changePasswordSuccess.innerHTML=`
+            <div class="text-center col-8 m-auto mt-5">
+                <i class="fa-solid fa-check fs-1 text-light p-3 rounded-circle" style="background-color:#590140"></i>
+                <h2 class="m-2 lh-base">Password successfully changed</h2>
+                <a href="/html/dashboard.html" class="btn text-light" style="background-color: #590140;">Go back to HOME</a>
+            </div>`
         }else{
             passwordToEnter.innerText = "Enter your New Password"
             passwords = []
@@ -41,12 +49,12 @@ const changePassword = () => {
             enterdedNewPassword = false
             // var enteredOldPassword = false
             // var enterdedNewPassword = false
-            alert("Noo")
+            alert("Passswords does not match")
         }
     }
     if(checkPasswordValidation==false){
         passwordTyped.classList.add("is-invalid")
-    }else{
+    }else if (checkPasswordValidation){
         passwordTyped.classList.remove("is-invalid")
     }
 }
