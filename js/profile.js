a5191cf0c9c983c3e7466a5f87720f27201d5658
TECHIE SAM <blessingabolade15@gmail.com>
2023-01-14 21:04:02 +0100

    Added (Upload profile photo feature)

--- js/profile.js
+++ js/profile.js
@@ -1,52 +1,72 @@
+
 /* Profile details page */
-    if (localStorage.currentUser && localStorage.usersList) {
-        allUsers = JSON.parse(localStorage.getItem("usersList"))
-        onlineUser = JSON.parse(localStorage.getItem("currentUser"))
-        dispAccName.innerText = `${onlineUser.firstname} ${onlineUser.lastname}`
-        console.log(onlineUser)
-    }
+var imageUrl
+if (localStorage.currentUser && localStorage.usersList) {
+    allUsers = JSON.parse(localStorage.getItem("usersList"))
+    onlineUser = JSON.parse(localStorage.getItem("currentUser"))
+    dispAccName.innerText = `${onlineUser.firstname} ${onlineUser.lastname}`
+    console.log(onlineUser)
+    imageUrl = localStorage.getItem("thumbnail")
+    profilePhoto.src = onlineUser.profilePhoto
+}else{
+    imageUrl
+}
 
-    // Display user details in inputs
-    const displayEditModal = () => {
-        fname.value = onlineUser.firstname
-        lname.value = onlineUser.lastname
-        email.value = onlineUser.email
-        phone.value = onlineUser.phoneNo
-    }
+console.log(imageUrl)
 
-    // Save edited details
-    const edit = () => {
-        onlineUser.firstname = fname.value
-        onlineUser.lastname = lname.value
-        onlineUser.email = email.value
-        onlineUser.phoneNo = phone.value
-        updateUser = allUsers.find((item, index) => item.accountnumber == onlineUser.accountnumber)
-        if (onlineUser.password == pass.value) {
-            updateUser.firstname = onlineUser.firstname
-            updateUser.lastname = onlineUser.lastname
-            updateUser.email = onlineUser.email
-            updateUser.phone = onlineUser.phone
-            updateUser.bvn = onlineUser.bvn
-            localStorage.setItem("usersList", JSON.stringify(allUsers))
-            localStorage.setItem('currentUser', JSON.stringify(onlineUser))
-            alert("Changes Saved")
-            window.location = "profile.html"
-        } else {
-            incorrectPassword.innerText = "Incorrect Password"
-            pass.classList.add("is-invalid")
-        }
-    }
+// Display user details in inputs
+const displayEditModal = () => {
+    fname.value = onlineUser.firstname
+    lname.value = onlineUser.lastname
+    email.value = onlineUser.email
+    phone.value = onlineUser.phoneNo
+}
 
-    // display BVN to users
-    const viewBvn = () => {
-        if (confirmBvn.value == onlineUser.password) {
-            dispBvn.innerText = `BVN: ${onlineUser.bvn}`
-            confirmBvn.value = ""
-        } else {
-            confirmBvn.classList.add("is-invalid")
-            invalidpassword.innerHTML = "Incorrect password"
-            alert("Incorrect Password")
-            confirmBvn.value = ""
-
-        }
+// Save edited details
+const edit = () => {
+    onlineUser.firstname = fname.value
+    onlineUser.lastname = lname.value
+    onlineUser.email = email.value
+    onlineUser.phoneNo = phone.value
+    updateUser = allUsers.find((item, index) => item.accountnumber == onlineUser.accountnumber)
+    if (onlineUser.password == pass.value) {
+        updateUser.firstname = onlineUser.firstname
+        updateUser.lastname = onlineUser.lastname
+        updateUser.email = onlineUser.email
+        updateUser.phone = onlineUser.phone
+        updateUser.bvn = onlineUser.bvn
+        localStorage.setItem("usersList", JSON.stringify(allUsers))
+        localStorage.setItem('currentUser', JSON.stringify(onlineUser))
+        alert("Changes Saved")
+        window.location = "profile.html"
+    } else {
+        incorrectPassword.innerText = "Incorrect Password"
+        pass.classList.add("is-invalid")
     }
+}
+// display BVN to user
+dispBvn.addEventListener("click", () => {
+    dispBvn.innerText = `BVN ${onlineUser.bvn}`
+    setTimeout(() => {
+        dispBvn.innerText = `View my BVN`
+    }, 5000);
+})
+    var imageFile
+    uploadedPhoto.addEventListener("change", (event)=>{
+        imageFile = event.target.files[0]
+        const generateUrl = new FileReader()
+        generateUrl.readAsDataURL(imageFile)
+        generateUrl.addEventListener('load', () => {
+            onlineUser.profilePhoto = generateUrl.result
+            console.log(generateUrl.result)
+            localStorage.setItem('currentUser',JSON.stringify(onlineUser));
+            window.location="profile.html"
+            // profilePhoto.src = generateUrl.result
+        });
+    })
+
+    $(document).ready(function(){
+        $("#myBtn").click(function(){
+          $("#updateProfilePhoto").modal();
+        });
+      });
